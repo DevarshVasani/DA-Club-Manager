775fbc3b15a6fc6f0acb0d42a94041a1c490bdb4
devarsh <vasanidevarsh@gmail.com>
2024-04-03 19:24:36 +0530

    submit request , auto fill and show request is updated

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,269 @@
+from cProfile import label
+from re import I
+from urllib import request
+import customtkinter as ctk
+from CTkListbox import *
+import tkinter
+import os
+
+import superfastcode2
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+root.title("DAIICT club manager")
+root.geometry("1020x700")
+
+
+
+def main():
+    
+    
+
+    def member_request():
+        root1 = ctk.CTk()
+        
+        root1.geometry("650x300")
+        root1.title("Send Request")
+            
+            
+        def request():
+            name_member = user_entry.get()
+            id_member = id_entry.get()
+            club_member = club_box.get()
+            
+            member.member_request(name_member, id_member, club_member)
+
+            root1.destroy()
+            list_name.append(name_member)
+            list_club.append(club_member)
+            list_id.append(id_member)
+            
+            update_request()
+            
+            
+            
+
+            
+
+        
+        
+
+        
+        user_entry = ctk.CTkEntry(root1, width=300, placeholder_text="Enter your name", border_width=2, font=('Helvetica', 16))
+        user_entry.place(relx = 0.5, rely = 0.2, anchor = "n")
+        
+
+        
+        id_entry = ctk.CTkEntry(root1, width=300, placeholder_text="Enter your ID", border_width=2, font=('Helvetica', 16))
+        id_entry.place(relx = 0.5, rely = 0.4, anchor = "n")
+        
+        list_clubs = ['quiz', 'chess', 'dance', 'music', 'drama', 'coding']
+        
+
+
+        club_label = ctk.CTkLabel(root1, text="Select club : ", font=("Times New Roman", 20))
+        club_label.place(relx = 0.2, rely = 0.6)
+        
+        box_var = ctk.StringVar(value=list_clubs[0])
+
+        club_box = ctk.CTkComboBox(root1, values=list_clubs)
+        club_box.place(relx = 0.5, rely = 0.6, anchor = "n")
+        
+        submit_button = ctk.CTkButton(root1, text="Send Request", corner_radius=50, width=70, text_color="black", command = request)
+        submit_button.place(relx = 0.5, rely = 0.8, anchor = "n")
+        
+        root1.mainloop()
+
+        
+    try:
+        for widget in root.winfo_children():
+            widget.destroy()
+    except:
+        pass
+    def check(e):
+        global listbox
+        try:
+            
+            listbox.destroy()
+        except:
+            pass
+        
+        listbox = CTkListbox(root, width = 700)
+        listbox.place(x=100, y = 175)
+        
+        type = search_bar.get()
+        
+        data =[]
+        i=0
+        
+        if type == '':
+            try:
+                listbox.destroy()
+            except:
+                pass
+        else:
+        
+            for item in autofill:
+                 if type.lower() in item.lower():
+                     listbox.insert(i, item)
+                     i = i+1
+                 
+
+         
+        
+        
+        
+    def search():
+        
+
+        name_search = search_bar.get()
+        
+        for widger in search_frame.winfo_children():
+            widger.destroy()
+            
+
+
+        if (name_search[0] == '2'):
+            list = member.search_by_id(name_search)
+        else:
+            list = member.search_by_name(name_search)
+            
+        
+    
+        print(list)
+        
+        name_label = ctk.CTkLabel(search_frame, text=f"Name : {list[0]}", font=("Times New Roman", 20))
+        name_label.pack()
+        
+        id_label = ctk.CTkLabel(search_frame, text=f"ID : {list[1]}", font=("Times New Roman", 20))
+        id_label.pack()
+        
+        club_label = ctk.CTkLabel(search_frame, text=f"Club : {list[2]}", font=("Times New Roman", 20))
+        club_label.pack()
+        
+        back_button = ctk.CTkButton(search_frame, text="Back", command=main)
+        back_button.pack()
+    
+
+
+    global list_club, list_id, list_name
+    member = superfastcode2.Member()
+    
+    title_frame = ctk.CTkFrame(root, width = 1020, height = 150, fg_color="#0f0f0f")
+    title_frame.place(relx = 0.5, rely = 0, anchor = "n")
+
+    title_label = ctk.CTkLabel(title_frame, text="DAIICT Club Manager", font=("Arial Black", 40))
+    title_label.place(relx = 0.5, rely = 0.25, anchor = "n")
+    
+
+    search_frame = ctk.CTkFrame(root, width=1020, height=71,fg_color= "#0f0f0f", border_width=0, corner_radius=0)
+
+    search_frame.place(relx = 0.5, rely = 0.17, anchor = "n")
+    
+    search_bar = ctk.CTkEntry(search_frame, width=700,corner_radius=50, placeholder_text="Search...", border_width=2, font=('Helvetica', 16))
+    search_bar.place(relx = 0.45, rely = 0.25, anchor = "n")
+    
+    search_bar.bind("<KeyRelease>", check)
+    
+    autofill = superfastcode2.autokeys()
+    
+    search_button = ctk.CTkButton(search_frame, text="Search", corner_radius=50,command=search, width=70, text_color="black")
+    search_button.place(relx = 0.83, rely = 0.25, anchor = "n")
+
+    access_frame = ctk.CTkFrame(root, width=280, height=500, border_width=0, fg_color="#0f0f0f",corner_radius=0)
+    access_frame.place(relx = 0.00, rely = 0.27, anchor = "nw")
+    
+    home_button = ctk.CTkButton(access_frame, text="Home", fg_color="#242424",cursor="hand2" ,width=250,height = 50, font=('Helvetica', 16))
+    home_button.place(relx = 0.05, rely = 0.15)
+    
+    club_button = ctk.CTkButton(access_frame, text="View Club", fg_color="#242424",cursor="hand2", width=250,height = 50)
+    club_button.place(relx = 0.05, rely = 0.3)
+
+    sendrequest_button = ctk.CTkButton(access_frame, text="Request to join club",fg_color="#242424",cursor="hand2", width=250,height = 50, command=member_request)
+    sendrequest_button.place(relx = 0.05, rely = 0.45)
+    
+    help_button = ctk.CTkButton(access_frame, text="Help",fg_color="#242424",cursor="hand2", width=250,height = 50)
+    help_button.place(relx = 0.05, rely = 0.6)
+    
+    request_frame = ctk.CTkScrollableFrame(root,fg_color="#0f0f0f", width=721, height=500, border_width=0,corner_radius=0)
+    request_frame.place(relx = 1, rely = 0.27, anchor = "ne")
+    
+    
+    
+    list_name = ["Meet", "dharmya", "Kalp", "Devarsh"]
+    list_id = ["201801001", "201801002", "201801003", "201801004"]
+    list_club = ["quiz", "chess", "dance", "music"]
+    def update_request():
+        global list_name, list_id, list_club
+        def accept_window(text):
+            
+            def adding():
+                entered_pass = password.get()
+                if entered_pass == "123456":
+                    pass
+                    
+
+            root2 = ctk.CTk()
+            root2.geometry("350x200")
+            root2.title("Accept Request")
+            
+            password = ctk.CTkEntry(root2, width=300, placeholder_text="Enter password", show = "*", border_width=2, font=('Helvetica', 16))
+            password.place(relx = 0.5, rely = 0.2, anchor = "n")
+            
+            submit_pass = ctk.CTkButton(root2, text="Submit", corner_radius=50, width=70, text_color="black")
+            submit_pass.place(relx = 0.5, rely = 0.5, anchor = "n")
+            
+            root2.mainloop()
+            
+            
+        
+    
+        for i in range(len(list_name)):
+            
+            def func(x=list_name[i]):
+                return accept_window(x)
+
+            
+
+            request_title = ctk.CTkLabel(request_frame, text="\tName\t\tID\t  Club", font=('Century Gothic', 20))
+            request_title.grid(row = 0, column = 0, pady=10, padx=10, sticky="w")
+
+            labelforall = ctk.CTkLabel(request_frame, text = f"{i + 1}.\t{list_name[i]}\t          {list_id[i]}\t   {list_club[i]}      ", font=('Century Gothic', 20))
+            labelforall.grid(row = i+1, column = 0, pady=10, padx=10, sticky="w")
+            
+            accept_button = ctk.CTkButton(request_frame, text="Accept", corner_radius=50, width=70, text_color="black", command=func)
+            accept_button.grid(row = i+1, column = 1, pady=10, padx=10)
+            
+            reject_button = ctk.CTkButton(request_frame, text="Reject", corner_radius=50, width=70, text_color="black")
+            reject_button.grid(row = i+1, column = 2, pady=10, padx=10)
+            
+           
+            
+    update_request()      
+        
+        
+        
+        
+        
+
+    
+
+    
+
+    
+    
+    
+    
+
+
+    
+
+    
+
+    
+main()
+
+root.resizable(False, False)
+
+root.mainloop()
---
+++ superfastcode2/module.cpp
@@ -0,0 +1,301 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <limits>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include<pybind11/complex.h>
+#include<pybind11/functional.h>
+
+using namespace std;
+
+class Member;
+
+unordered_map<string, Member> buildHashTable(const string& myfile, const string& key);
+
+class Member {
+public:
+    string name;
+    string id;
+    string club;
+
+
+
+
+
+    // Default constructor
+    Member() : name(""), id(""), club("") {}
+
+    // Parameterized constructor
+    Member(const string& name, const string& id, const string& club)
+        : name(name), id(id), club(club) {}
+
+    vector<string> searchbyName(string& key) {
+        vector<string>identity(3);  
+
+        unordered_map<string, Member> hashtable = buildHashTable("D://capstone//Records.csv", "name");
+        auto it = hashtable.find(key);
+        if (it != hashtable.end()) {
+            cout << "Member found!" << endl;
+            cout << "Name: " << it->second.name << endl;
+            cout << "ID: " << it->second.id << endl;
+            cout << "Club: " << it->second.club << endl;
+            identity[0] = it->second.name;
+            identity[1] = it->second.id;
+            identity[2] = it->second.club;
+        }
+        else {
+            cout << "Member not found :(" << endl;
+            
+        }
+        return identity;
+    }
+
+    vector<string> searchbyID(string key) {
+        vector<string>id(3);
+
+        unordered_map<string, Member> hashtable = buildHashTable("D://capstone//Records.csv", "id");
+        auto it = hashtable.find(key);
+        if (it != hashtable.end()) {
+            cout << "Member found!" << endl;
+            cout << "Name: " << it->second.name << endl;
+            cout << "ID: " << it->second.id << endl;
+            cout << "Club: " << it->second.club << endl;
+            id[0] = it->second.name;
+            id[1] = it->second.id;
+            id[2] = it->second.club;
+        }
+        else {
+            cout << "Member not found :(" << endl;
+        }
+        return id;
+    }
+
+
+    // Delete member function
+    void deleteMemberByName() {
+        string password;
+        cout << "Enter password (consists of six digits): ";
+        cin >> password;
+        if (password != "123456") {
+            cout << "Incorrect password. Access denied." << endl;
+            return;
+        }
+
+        ifstream inFile("D://capstone//Records.csv");
+        ofstream tempFile("temp.csv");
+        string memberName;
+        cout << "Enter the name of the member to be deleted: ";
+        cin >> memberName;
+        if (inFile.is_open() && tempFile.is_open()) {
+            string line;
+            while (getline(inFile, line)) {
+                istringstream iss(line);
+                string n, i, c;
+                getline(iss, n, ',');
+                getline(iss, i, ',');
+                getline(iss, c);
+                // Check if the name contains the memberName as a substring
+                if (n.find(memberName) == string::npos) {
+                    tempFile << line << endl;
+                }
+            }
+            inFile.close();
+            tempFile.close();
+            remove("D://capstone//Records.csv");
+            rename("temp.csv", "D://capstone//Records.csv");
+            cout << "Member deleted successfully." << endl;
+        }
+        else {
+            cerr << "Failed to open CSV file." << endl;
+        }
+        cout << "Member deleted successfully" << endl;
+        return;
+    }
+
+    void acceptRequests(string name, string id, string club) {
+
+        ifstream requestFile("D://capstone//Requests.csv");
+        if (!requestFile.is_open()) {
+            cerr << "Failed to open Requests file." << endl;
+            return;
+        }
+
+        //getline(requestFile, name, ',');
+        //getline(requestFile, id, ',');
+        //getline(requestFile, club);
+
+        requestFile.close();
+        cout << "Showing the earliest sent request:" << endl;
+        cout << "Member Name: " << name << endl;
+        cout << "Member ID: " << id << endl;
+        cout << "Member Club: " << club << endl;
+        string ans;
+        cout << "Do you want to accept the request? ";
+        cin >> ans;
+
+        if (ans == "Yes" || ans == "yes") {
+
+            string password;
+            cout << "Enter password to accept the request: ";
+            cin >> password;
+            if (password != "123456") {
+                cout << "Incorrect password. Request not accepted." << endl;
+                return;
+            }
+
+            ofstream recordsFile("D://capstone//Records.csv", ios::app);
+            if (!recordsFile.is_open()) {
+                cerr << "Failed to open Records file." << endl;
+                return;
+            }
+
+            recordsFile << name << "," << id << "," << club << endl;
+
+            recordsFile.close();
+
+            //deleteMemberRequest(name);
+            cout << "Request accepted successfully." << endl;
+        }
+        else {
+            cout << "Sorry your request has been rejected :(" << endl;
+            //deleteMemberRequest(name);
+        }
+    }
+
+    // Insert member function
+    void insert() {
+
+        string password;
+        cout << "Enter password (consists of 6 digits): ";
+        cin >> password;
+
+
+        if (password != "123456") {
+            cout << "Incorrect password. Access denied." << endl;
+            return;
+        }
+
+
+        cout << "Enter name of the member: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Enter ID of the member: ";
+        getline(cin, id);
+        cout << "Enter club of the member: ";
+        getline(cin, club);
+
+
+        ofstream file("D://capstone//Records.csv", ios::app);
+        if (!file.is_open()) {
+            cerr << "Failed to open CSV file." << endl;
+            return;
+        }
+
+
+        file << name << "," << id << "," << club << endl;
+        file.close();
+
+        cout << "Member added successfully." << endl;
+    }
+
+    string sendRequests(string name, string id, string club) {
+
+        //cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        //cout << "Enter name of the member: ";
+        //getline(cin, name);
+
+        //cout << "Enter ID of the member: ";
+        //getline(cin, id);
+        //cout << "Enter club of the member: ";
+        //getline(cin, club);
+
+
+        ofstream file("D://capstone//Requests.csv", ios::app);
+        if (!file.is_open()) {
+            cerr << "Failed to open CSV file." << endl;
+            return "Failed to open CSV file.";
+        }
+
+
+        file << name << "," << id << "," << club << endl;
+        file.close();
+
+        cout << "Request sent successfully." << endl;
+        return "Request sent successfully.";
+    }
+};
+
+unordered_map<string, Member> buildHashTable(const string& myfile, const string& key) {
+    ifstream file(myfile);
+    unordered_map<string, Member> hashtable;
+
+    if (!file.is_open()) {
+        cout << "File doesn't exist" << endl;
+        return hashtable;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
+        string name, id, club;
+
+        getline(iss, name, ',');
+        getline(iss, id, ',');
+        getline(iss, club);
+
+        Member student{ name, id, club };
+        string k = (key == "id") ? id : (key == "club") ? club : name;
+
+        hashtable[k] = student;
+    }
+
+    file.close();
+    return hashtable;
+}
+void printHashTable(const unordered_map<string, Member>& hashtable) {
+    for (const auto& pair : hashtable) {
+        cout << "Key: " << pair.first
+            << " | Name: " << pair.second.name
+            << ", ID: " << pair.second.id
+            << ", Club: " << pair.second.club << endl;
+    }
+}
+vector<string> returnKey() {
+    vector<string> v;
+    unordered_map<string, Member> hashtable = buildHashTable("D://capstone//Records.csv", "name");
+    for (const auto& pair : hashtable) {
+        v.push_back(pair.first);
+    }
+    return v;
+}
+
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(superfastcode2, m) {   
+
+    py::class_<Member>(m, "Member")
+        .def(py::init<>())
+        .def("search_by_id", &Member::searchbyID)
+        .def("member_request", &Member::sendRequests)
+
+        .def("search_by_name", &Member::searchbyName);
+
+
+
+
+    m.def("autokeys", &returnKey, R"pbdoc(
+        Compute a hyperbolic tangent of a single argument expressed in radians.
+    )pbdoc");
+
+#ifdef VERSION_INFO 
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
